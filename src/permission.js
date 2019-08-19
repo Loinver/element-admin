a01402413d50308c84bb0aafea0274d38654f302
Linyer <loinverbaby@gmail.com>
2019-08-19 16:53:21 +0800

    备份测试

--- src/permission.js
+++ src/permission.js
@@ -1,53 +1,53 @@
 import router from './router'
 import store from './store'
-import {Message} from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import {getToken} from '@/utils/auth' // get token from cookie
+import { Message } from 'element-ui'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { getToken } from '@/utils/auth' // cookie中获取token
 
-NProgress.configure({showSpinner: false}) // NProgress Configuration
+NProgress.configure({ showSpinner: false }) // 进度条设置
 
-// permission judge function
+// 权限判断方法
 function hasPermission(roles, permissionRoles) {
-  if (roles.includes('admin')) return true // admin permission passed directly
+  if (roles.includes('admin')) return true // 用户是管理员权限
   if (!permissionRoles) return true
   return roles.some(role => permissionRoles.indexOf(role) >= 0)
 }
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+const whiteList = ['/login', '/auth-redirect'] // 白名单 不验证权限
 
 router.beforeEach((to, from, next) => {
-  NProgress.start() // start progress bar
+  NProgress.start() // 开始进度条
   if (getToken()) {
     // determine if there has token
     // 有token
     if (to.path === '/login') {
-      next({path: '/'})
-      NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
+      next({ path: '/' })
+      NProgress.done()
     } else {
       if (store.getters.roles.length === 0) {
         // 判断当前用户是否已拉取完user_info信息
         store.dispatch('GetUserInfo')
           .then(res => {
             // 拉取user_info
-            const roles = res.data.roles; // note: roles must be a object array! such as: [{id: '1', name: 'editor'}, {id: '2', name: 'developer'}]
-            store.dispatch('GenerateRoutes', {roles}).then(accessRoutes => {
+            const roles = res.data.roles; // 权限必须是一个对象数组  比如a: [{id: '1', name: 'editor'}, {id: '2', name: 'developer'}]
+            store.dispatch('GenerateRoutes', { roles }).then(accessRoutes => {
               // 根据roles权限生成可访问的路由表
               router.addRoutes(accessRoutes) // 动态添加可访问路由表
-              next({...to, replace: true}) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+              next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
             })
           }).catch(err => {
-          store.dispatch('FedLogOut').then(() => {
-            Message.error(err)
-            next({path: '/'})
+            store.dispatch('FedLogOut').then(() => {
+              Message.error(err)
+              next({ path: '/' })
+            })
           })
-        })
       } else {
         // 没有动态改变权限的需求可直接next() 删除下方权限判断 ↓
         if (hasPermission(store.getters.roles, to.meta.roles)) {
           next()
         } else {
-          next({path: '/401', replace: true, query: {noGoBack: true}})
+          next({ path: '/401', replace: true, query: { noGoBack: true } })
         }
         // 可删 ↑
       }
